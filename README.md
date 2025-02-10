7c953d7ee71f51b06f05397d8084ba59c9c37623
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-02-09 21:58:12 -0500

    user authentication

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ db.create_all()
 to experiment with queries:
 from flaskblog.models import User, Post
 User.query.all()
+
+start virtual env: source venv/bin/activate
+
+macos airplay listening on port 5000
+lsof -i :5000 to check
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -8,5 +10,9 @@ app.config['SECRET_KEY'] = '900508fd00cb981a1f2587ded1f215ef'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'site.db')}"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
