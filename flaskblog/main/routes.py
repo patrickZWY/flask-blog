534e3765edb6222e278be6e35970121e66b45f05
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-02-18 19:28:12 -0500

    add latest three posts to side panel

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -8,9 +8,15 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    latest_posts = Post.query.order_by(Post.date_posted.desc()).limit(3).all()
+    return render_template('home.html', posts=posts, latest_posts=latest_posts)
 
 
 @main.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@main.app_context_processor
+def inject_latest_posts():
+    latest_posts = Post.query.order_by(Post.date_posted.desc()).limit(3).all()
+    return dict(latest_posts=latest_posts)
